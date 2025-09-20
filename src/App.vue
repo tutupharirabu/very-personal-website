9bdbd9329b881f3c418ff756a55f83f02289d383
Irfan Zharauri Nanda Sudiyanto <code.zharaurien@gmail.com>
2025-09-21 01:04:41 +0700

    First Commit! - Finishing Setting Up

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <h1>You did it! from Vue</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <h1 class="text-3xl font-bold underline">
+    Hello world! from Tailwindcss
+  </h1>
+</template>
+
+<style scoped></style>
